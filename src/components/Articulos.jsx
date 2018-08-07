32513f66f22bcab170e5390d8d807777b811a58b
ErickVAleman <Erickva70@gmail.com>
2018-08-06 20:01:08 -0500

    update

--- src/components/Articulos.jsx
+++ src/components/Articulos.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Table, Button, Modal, Alert, Input } from 'antd';
+import Item from '../../node_modules/antd/lib/list/Item';
 const { Search } = Input
 const apiList = `http://127.0.0.1:3001/api/v1/consulta/articulos`;
 
@@ -7,13 +8,22 @@ const apiList = `http://127.0.0.1:3001/api/v1/consulta/articulos`;
 class ListaArticulos extends Component {
   state = {
     Lista: [],
-    Modal: false
+    Modal: false,
+    SearchText: '',
   }
 
   columns = [
     { title: 'Articulo', dataIndex: 'Articulo', width: '20%' },
     { title: 'CodigoBarras', dataIndex: 'CodigoBarras', width: '20%' },
-    { title: 'Nombre', dataIndex: 'Nombre', width: '20%' },
+    { 
+      title: 'Nombre', 
+      dataIndex: 'Nombre', 
+      width: '20%',
+      render: (text) => {
+        const { SearchText } = this.state;
+        return SearchText ? (<span> { text.split(new RegExp(`(?${SearchText})|(?${SearchText})`, 'i')).map((item, index)=>{console.log(item,index)}) } </span>) : 0
+      }
+    },
     { title: 'Descripcion', dataIndex: 'Descripcion', width: '20%' },
     { title: 'Relacion', dataIndex: 'Relacion', width: '20%' },
     { title: 'Ver', width: '20%', fixed: 'rigth', render: (key) => <Button key={key} type="dashed" >Ver</Button> }
@@ -34,6 +44,10 @@ class ListaArticulos extends Component {
     }
   }
 
+  onSearch = (SearchText) => {
+    this.setState({SearchText})
+  }
+
   onModal() {
     this.setState({ Modal: !this.state.Modal })
   }
@@ -41,23 +55,14 @@ class ListaArticulos extends Component {
   render() {
     return (
       <div>
-        <Modal
-          title="Vertically centered modal dialog"
-          wrapClassName="vertical-center-modal"
-          visible={this.state.visible}
-        >
-          <p>some contents...</p>
-          <p>some contents...</p>
-          <p>some contents...</p>
-        </Modal>
         <Search
           placeholder="input search text"
-          onSearch={value => console.log(value)}
+          onSearch={(data) => this.onSearch(data)}
           style={{ width: 200 }}
         />
         <br/>
         <br/>
-        <Table columns={this.columns} dataSource={this.state.Lista} />
+        <Table columns={this.columns} dataSource={this.state.Lista} bordered />
       </div>
     )
   }
