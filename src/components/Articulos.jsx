c7b60a72b4602e1d7ea06b26945f715cc5503a2a
Erick <Erickva70@gmail.com>
2018-08-11 15:48:44 -0500

    front lista

--- src/components/Articulos.jsx
+++ src/components/Articulos.jsx
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import { Table, Button, Modal, Alert, Input, Pagination } from 'antd';
+import { Table, Button, Modal, Card, Input, Row, Col } from 'antd';
 const { Search } = Input
 const apiList = `http://192.168.123.63:3001/api/v1/consulta/articulos`;
 
 
 class ListaArticulos extends Component {
   state = {
-    data : [],
+    data: [],
     filterDropdownVisible: false,
     searchText: '',
     loading: false,
-    pagination: {}
+    pagination: {},
+    itemdata: {},
+    modal2Visible: false
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.setState({ loading: true });
-    if(localStorage.getItem('data')){
+    if (localStorage.getItem('data')) {
       const data = localStorage.getItem('data')
       const pagination = { ...this.state.pagination };
       pagination.position = 'both';
-     
+
       this.setState(
         {
           data: JSON.parse(data),
@@ -30,32 +32,67 @@ class ListaArticulos extends Component {
       return;
     }
     fetch(apiList)
-    .then(data => data.json())
-    .then(lista => {
-      const pagination = { ...this.state.pagination };
-      pagination.position = 'both';
-      localStorage.setItem('data', JSON.stringify(lista));
-      console.log(lista)
-      const data = localStorage.getItem('data')
-      this.setState(
-        {
-          data: JSON.parse(data),
-          loading: false,
-          pagination
-        }
-      )
-      return;
-    })
-    .catch(e => {
-      console.log(e)
-    })
+      .then(data => data.json())
+      .then(lista => {
+        const pagination = { ...this.state.pagination };
+        pagination.position = 'both';
+        localStorage.setItem('data', JSON.stringify(lista));
+        console.log(lista)
+        const data = localStorage.getItem('data')
+        this.setState(
+          {
+            data: JSON.parse(data),
+            loading: false,
+            pagination
+          }
+        )
+        return;
+      })
+      .catch(e => {
+        console.log(e)
+      })
   }
 
   onInputChange = (e) => {
-    console.log()
     this.setState({ searchText: e.target.value });
   }
 
+  onClickChangeItem = (e) => {
+    this.setState({ itemdata: e });
+  }
+
+  setModal2Visible() {
+    this.setState({ modal2Visible: !this.state.modal2Visible });
+  }
+
+  fetchArticulo = async (e) => {
+    try {
+      const metadata = await fetch(e.URL);
+      try {
+        const data = await metadata.json();
+        this.setState({ itemdata: data })
+        this.setModal2Visible()
+        console.log(this.state.itemdata)
+      } catch (e) {
+        console.log(e)
+      }
+    } catch (e) {
+      console.log(e)
+    }
+
+  }
+
+  onRenderModal = () => (
+    <Modal
+      title="Hola"
+      centered
+      visible={this.state.modal2Visible}
+    >
+      <p>Hola</p>
+      <p>some contents...</p>
+      <p>some contents...</p>
+    </Modal>
+  )
 
   onSearch = () => {
     const { searchText } = this.state;
@@ -96,43 +133,156 @@ class ListaArticulos extends Component {
 
   render() {
     const columns = [
-      { title: 'Articulo', dataIndex: 'Articulo', width: '10%' },
-      { title: 'CodigoBarras', dataIndex: 'CodigoBarras', width: '14%' },
+      { title: 'Articulo', dataIndex: 'Articulo', width: '20%' },
+      { title: 'CodigoBarras', dataIndex: 'CodigoBarras', width: '20%' },
       {
         title: 'Nombre',
         dataIndex: 'Nombre',
         width: '20%',
-        filterDropdown: (
-          <div className="custom-filter-dropdown">
-            <Input
-              placeholder="Search name"
-              value={this.state.searchText}
-              onChange={this.onInputChange}
-              onPressEnter={this.onSearch}
-            />
-            <Button type="primary" onClick={this.onSearch}>Search</Button>
-          </div>
-        ),
         filterDropdownVisible: this.state.filterDropdownVisible,
         onFilterDropdownVisibleChange: visible => this.setState({ filterDropdownVisible: visible }),
-  
+
       },
-      { title: 'Descripcion', dataIndex: 'Descripcion', width: '30%' },
+      { title: 'Descripcion', dataIndex: 'Descripcion', width: '20%' },
       { title: 'Relacion', dataIndex: 'Relacion', width: '20%' },
       {
-        title: 'Ver', width: '20%', fixed: 'rigth',
+        title: 'Ver',
+        width: '20%',
+        fixed: 'rigth',
+        render: (e) => {
+          return <Button type="primary" shape="circle" icon="search" onClick={() => this.fetchArticulo(e)} />
+        }
       }
     ]
+
+    const columnsCompras = [{
+      title: 'Cajas',
+      dataIndex: 'CantidadRegularUC',
+      key: 'CantidadRegularUC',
+      with: '20%'
+    }, {
+      title: 'Costo por caja',
+      dataIndex: 'CostoUnitarioNetoUC',
+      key: 'CostoUnitarioNetoUC',
+      with: '20%'
+    }, {
+      title: 'Fecha',
+      dataIndex: 'Fecha',
+      key: 'Fecha',
+      with: '20%'
+    }, {
+      title: 'NombreTercero',
+      dataIndex: 'NombreTercero',
+      key: 'NombreTercero',
+      with: '20%'
+    }];
+
+    const columnsExistencias = [
+      {
+        title: 'Suc',
+        dataIndex: 'Suc',
+        key: 'Suc',
+        with: '20%',
+        fixed: 'left'
+      },
+      {
+        title: 'CostoExist',
+        dataIndex: 'CostoExist',
+        key: 'CostoExist',
+        with: '20%'
+      },
+      {
+        title: 'CostoNet',
+        dataIndex: 'CostoNet',
+        key: 'CostoNet',
+        with: '20%'
+      },
+      {
+        title: 'CostoNetUC',
+        dataIndex: 'CostoNetUC',
+        key: 'CostoNetUC',
+        with: '20%'
+      },
+      {
+        title: 'ExistUC',
+        dataIndex: 'ExistUC',
+        key: 'ExistUC',
+        with: '20%'
+      },
+      {
+        title: 'ExistUV',
+        dataIndex: 'ExistUV',
+        key: 'ExistUV',
+        with: '20%'
+      },
+      {
+        title: 'Stock30',
+        dataIndex: 'Stock30',
+        key: 'Stock30',
+        with: '20%'
+      },
+      {
+        title: 'Stock30UC',
+        dataIndex: 'Stock30UC',
+        key: 'Stock30UC',
+        with: '20%'
+      },
+    ]
+    const { itemdata } = this.state
     return (
       <div>
-        <Table 
+        <Modal
+          title={itemdata.Nombre}
+          centered
+          visible={this.state.modal2Visible}
+          onOk={() => this.setModal2Visible()}
+          width="70%"
+        >
+          <div style={{ background: '#ECECEC', padding: '30px' }} >
+            <Row gutter={16} >
+              <Col span={12} >
+                <Card >
+                  <p>Articulo: <span className="highlight">{itemdata.Articulo}</span></p>
+                  <p>Nombre: <span className="highlight">{itemdata.Nombre}</span></p>
+                  <p>Relacion: <span className="highlight">{itemdata.Relacion}</span></p>
+                  <p>CostoExistActual: <span className="highlight">{itemdata.CostoExistActual}</span></p>
+                </Card>
+              </Col>
+              <Col span={12} >
+                <Card >
+                  <p>CostoNetUCBO: <span className="highlight">{itemdata.CostoNetUCBO}</span></p>
+                  <p>ExistActualUC: <span className="highlight">{itemdata.ExistActualUC}</span></p>
+                  <p>Stock30UC: <span className="highlight">{itemdata.Stock30UC}</span></p>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+          <div>
+            <br/>
+            <Table dataSource={itemdata.compras} columns={columnsCompras} bordered scroll={{ x: 500 }} title={() => 'Compras'} />
+            <Table dataSource={itemdata.existencias} columns={columnsExistencias} bordered scroll={{ x: 750 }} title={() => 'Existencias'} />
+          </div>
+        </Modal>
+
+        <Search
+          placeholder="Buscar Articulo"
+          enterButton="Buscar"
+          size="large"
+          value={this.state.searchText}
+          onChange={this.onInputChange}
+          onPressEnter={this.onSearch}
+          onSearch={this.onSearch}
+        />
+        <br />
+        <br />
+        <Table
           rowKey={(record, i) => i}
-          columns={columns} 
-          dataSource={this.state.data} 
-          bordered 
+          columns={columns}
+          dataSource={this.state.data}
+          bordered
           pagination={this.state.pagination}
           loading={this.state.loading}
-          />
+        />
       </div>
     )
   }
