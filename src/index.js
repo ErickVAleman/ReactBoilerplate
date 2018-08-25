17f90a469ecbce5a05ff8429367a2a8795693fca
Erick <Erickva70@gmail.com>
2018-08-25 14:48:48 -0500

    update sw

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render as Render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';1
 import App from './App.jsx'
-import sw from './sw'
 
 Render(
   <Router basename='/'>
